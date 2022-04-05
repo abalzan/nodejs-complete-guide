2ad8892753f5860054a19cf89cff0da97479e53d
Andrei Balzan <andreibalzan@gmail.com>
2022-04-05 08:54:08 +0100

    adding EJS layout

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-app.set('view engine', 'pug');
+
+//enable pug
+app.set('view engine', 'ejs');
 app.set('views','views');
 
 const adminData = require('./routes/admin');
@@ -16,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: null });
 });
 
 app.listen(3000)
