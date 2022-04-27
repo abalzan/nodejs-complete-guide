14c61394123176246786d7a503d3ad4d01f6f4ec
Andrei Balzan <andreibalzan@gmail.com>
2022-04-27 20:29:42 +0100

    installing mongodb driver and creating mongo db atlas connection

--- util/database.js
+++ util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require('sequelize');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const sequelize = new Sequelize('nodejs', 'nodejs', 'nodejs', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+const uri = "mongodb+srv://andrei:R9LeoQS78jgS0g04@nodejscompletecourseclu.xgbdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-module.exports = sequelize;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const mongoConnect = (callback) => {
+    client.connect()
+    .then(clientCb => {
+        console.log('Connected to MongoDB');
+        callback(clientCb);
+    }).catch(error => {
+        console.log(error);
+    });
+};
+
+module.exports = mongoConnect;
