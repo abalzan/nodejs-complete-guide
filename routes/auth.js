e99dc7c7fbd683c8da5eacb0ce57a135b08fedf3
Andrei Balzan <andreibalzan@gmail.com>
2022-05-26 10:34:48 +0100

    implementing token logic to reset password

--- routes/auth.js
+++ routes/auth.js
@@ -10,4 +10,5 @@ router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
 router.post('/signup', authController.postSignup);
 router.get('/reset', authController.getReset);
+router.post('/reset', authController.postReset);
 module.exports = router;
