201287c09c256bd2f7b87c79f4a3f84de4f45e32
Andrei Balzan <andreibalzan@gmail.com>
2022-05-29 10:01:40 +0100

    login validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,13 +32,26 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-  User.findOne({ email: email })
-      .then(user => {
-          if (!user) {
-              req.flash('error', 'Invalid email or password.');
-              return res.redirect('/login');
-          }
-          bcrypt.compare(password, user.password)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).render('auth/login', {
+            path: '/login',
+            pageTitle: 'Login',
+            errorMessage: errors.array()[0].msg,
+            oldInput: {
+                email: email,
+                password: password
+            },
+            validationErrors: errors.array()
+        });
+    }
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                req.flash('error', 'Invalid email or password.');
+                return res.redirect('/login');
+            }
+           bcrypt.compare(password, user.password)
               .then(doMatch => {
                   if (doMatch) {
                       req.session.isLoggedIn = true;
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,31 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { check } = require('express-validator');
 
 const authController = require('../controllers/auth');
-const User = require('../models/user');
+const User = require("../models/user");
 
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
-router.post('/login', authController.postLogin);
+router.post('/login',[
+    check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email address.')
+        .custom((value, { req }) => {
+            // if the user is already logged in, don't let them sign in again
+            if (req.session.isLoggedIn) {
+                return Promise.reject('You are already logged in!');
+            }
+            // if the user is not logged in, check if the email is already in the database
+            return User.findOne({email: value}).then((user) => {
+                if (!user) {
+                    return Promise.reject('Invalid email or password.');
+                }
+            })
+        }).normalizeEmail(),
+    check('password', 'Your password, must be at least 6 characters long.').isLength({ min: 6 }),
+], authController.postLogin);
 router.post('/logout', authController.postLogout);
 router.post('/signup',
     check('email').isEmail().withMessage('Please enter a valid email')
@@ -18,7 +35,7 @@ router.post('/signup',
                     return Promise.reject('Email address already exists');
                 }
             });
-        }),
+        }).normalizeEmail(),
     check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
     check('confirmPassword').custom((value, { req }) => {
         if (value !== req.body.password) {
