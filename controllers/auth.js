f58e6fb1b06ce06d80794b6b93083a3701c151de
Andrei Balzan <andreibalzan@gmail.com>
2022-05-30 08:28:18 +0100

    add validation to sigup/login

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,6 +22,7 @@ exports.getLogin = (req, res, next) => {
       email: '',
       password: ''
     },
+    validationErrors: []
   });
 };
 
@@ -35,6 +36,7 @@ exports.getSignup = (req, res, next) => {
             password: '',
             confirmPassword: ''
         },
+        validationErrors: []
     });
 };
 
@@ -57,8 +59,16 @@ exports.postLogin = (req, res, next) => {
     User.findOne({ email: email })
         .then(user => {
             if (!user) {
-                req.flash('error', 'Invalid email or password.');
-                return res.redirect('/login');
+                return res.status(422).render('auth/login', {
+                    path: '/login',
+                    pageTitle: 'Login',
+                    errorMessage: 'Invalid email or password.',
+                    oldInput: {
+                        email: email,
+                        password: password
+                    },
+                    validationErrors: []
+                });
             }
            bcrypt.compare(password, user.password)
               .then(doMatch => {
@@ -70,8 +80,16 @@ exports.postLogin = (req, res, next) => {
                           res.redirect('/');
                       });
                   }
-                  req.flash('error', 'Invalid email or password.');
-                  res.redirect('/login');
+                  return res.status(422).render('auth/login', {
+                      path: '/login',
+                      pageTitle: 'Login',
+                      errorMessage: 'Invalid email or password.',
+                      oldInput: {
+                          email: email,
+                          password: password
+                      },
+                      validationErrors: []
+                  });
               })
               .catch(err => {
                   console.log(err);
