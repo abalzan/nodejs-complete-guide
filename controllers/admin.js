ab29823d0d5d8458244a505ccbe05c397f0654e5
Andrei Balzan <andreibalzan@gmail.com>
2022-05-31 08:38:18 +0100

    add validation add and edit products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,4 @@
+const {validationResult} = require('express-validator');
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
@@ -5,7 +6,10 @@ exports.getAddProduct = (req, res, next) => {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
         editing: false,
-    })
+        hasError: false,
+        errorMessage: null,
+        validationErrors: [],
+});
 };
 
 exports.postAddProduct = (req, res, next) => {
@@ -13,6 +17,23 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).render('admin/edit-product', {
+            pageTitle: 'Add Product',
+            path: '/admin/add-product',
+            editing: false,
+            hasError: true,
+            product: {
+                title: title,
+                imageUrl: imageUrl,
+                price: price,
+                description: description
+            },
+            errorMessage: errors.array()[0].msg
+        })
+    }
     const product = new Product( {title: title, price: price, description: description, imageUrl: imageUrl, userId: req.user});
     product.save().then(() => {
         res.redirect('/admin/products');
@@ -27,6 +48,25 @@ exports.postEditProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: true,
+            hasError: true,
+            product: {
+                title: title,
+                imageUrl: imageUrl,
+                price: price,
+                description: description,
+                _id: id
+
+            },
+            errorMessage: errors.array()[0].msg,
+            validationErrors: errors.array()
+        })
+    }
     Product.findById(id).then(product => {
         if(product.userId.toString() !== req.user._id.toString()) {
             return res.redirect('/');
@@ -79,6 +119,9 @@ exports.getEditProduct = (req, res, next) => {
                 path: '/admin/edit-product',
                 editing: editMode,
                 product: product,
+                hasError: false,
+                errorMessage: null,
+                validationErrors: [],
             });
         })
         .catch(err => console.log(err));
--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
+const {body} = require('express-validator');
 
 const router = express.Router();
 
@@ -9,11 +10,21 @@ router.get('/add-product', isAuth, adminController.getAddProduct);
 
 router.get('/products', isAuth, adminController.getProducts);
 
-router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post('/add-product', [
+    body('title').isString().isLength({min: 3}).trim().withMessage('Title must be at least 3 characters long'),
+    body('imageUrl').isURL().withMessage('Image URL must be valid'),
+    body('price').isFloat().withMessage('Price must be a number'),
+    body('description').isLength({min: 5, max: 400}).trim().withMessage('Description must be between 5 and 400 characters long')
+    ],isAuth, adminController.postAddProduct);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post('/edit-product', [
+    body('title').isString().isLength({min: 3}).trim().withMessage('Title must be at least 3 characters long'),
+    body('imageUrl').isURL().withMessage('Image URL must be valid'),
+    body('price').isFloat().withMessage('Price must be a number'),
+    body('description').isLength({min: 5, max: 400}).trim().withMessage('Description must be between 5 and 400 characters long')
+    ], isAuth, adminController.postEditProduct);
 
 router.post('/delete-product', isAuth, adminController.deleteProduct);
 
