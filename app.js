7619e3843adf38e69be0b564dc45540cb1e58735
Andrei Balzan <andreibalzan@gmail.com>
2022-06-03 11:13:23 +0100

    add error handling

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.set('views','views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
+const {render500Error} = require("./controllers/error-handler");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -42,6 +43,12 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
+
+app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
   }
@@ -54,14 +61,8 @@ app.use((req, res, next) => {
         next();
     })
     .catch(err => {
-        throw new Error(err);
+        next(new Error(err));
     });
-});
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
 });
 
 app.use('/admin', adminRoutes.routes);
@@ -72,7 +73,11 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-    res.redirect('/500');
+    res.status(500).render('500', {
+        pageTitle: 'Error!',
+        path: '/500',
+        isAuthenticated: req.session.isLoggedIn
+    });
 });
 
 mongoose.connect(MONGODB_URI)
