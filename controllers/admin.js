6900f3f16056671f58c056e07ea1ff7dd2266f00
Andrei Balzan <andreibalzan@gmail.com>
2022-06-03 13:54:27 +0100

    handling file uploads with multer

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,10 +15,12 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
+    const image = req.file;
     const price = req.body.price;
     const description = req.body.description;
     const errors = validationResult(req);
+
+    console.log(image);
 
     if (!errors.isEmpty()) {
         return res.status(422).render('admin/edit-product', {
@@ -28,7 +30,7 @@ exports.postAddProduct = (req, res, next) => {
             hasError: true,
             product: {
                 title: title,
-                imageUrl: imageUrl,
+                image: image,
                 price: price,
                 description: description
             },
