122948e0084b2f48f0521fef7151b374e3635ed4
Andrei Balzan <andreibalzan@gmail.com>
2022-06-03 15:43:33 +0100

    generating PDF files with Order data using PDFKit

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,6 +3,7 @@ const Order = require('../models/order');
 const {render500Error} = require("./error-handler");
 const fs = require('fs');
 const path = require('path');
+const pdfDocument = require('pdfkit');
 
 exports.getProducts = (req, res, next) => {
     Product.find()
@@ -128,11 +129,24 @@ exports.getInvoice = (req, res, next) => {
             }
             const invoiceName = 'invoice-' + orderId + '.pdf';
             const invoicePath = path.join('data', 'invoices', invoiceName); // path /data/invoices/invoice-orderId.pdf
-            const file = fs.createReadStream(invoicePath);
+
+            const pdfDoc = new pdfDocument();
             res.setHeader('Content-Type', 'application/pdf');
             res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
-            file.pipe(res);
-
+            pdfDoc.pipe(fs.createWriteStream(invoicePath));
+            pdfDoc.pipe(res);
+            pdfDoc.fontSize(26).text('Invoice', {
+                underline: true
+            });
+            pdfDoc.text('---------------------');
+            let totalPrice = 0;
+            order.products.forEach(prod => {
+                totalPrice += prod.quantity * prod.product.price;
+                pdfDoc.fontSize(14).text(prod.product.title + ' - ' + prod.quantity + ' x ' + '$' + prod.product.price);
+            });
+            pdfDoc.text('---');
+            pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
+            pdfDoc.end();
 
         }).catch(err => {
         return render500Error(err, req, res, next)
