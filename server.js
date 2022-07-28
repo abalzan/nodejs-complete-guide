75df3b2087adca77ede96474ef34fadfc74b6554
Andrei Balzan <andreibalzan@gmail.com>
2022-07-28 08:02:19 +0100

    setting up ssl

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const http = require('http');
+const https = require('https');
 const app = require("./app");
+const fs = require("fs");
+const path = require('path');
 
-const server = http.createServer(app);
+const privateKey = fs.readFileSync(path.resolve(__dirname, './.ssl/server.key'));
+const certificate = fs.readFileSync(path.resolve(__dirname, './.ssl/server.cert'));
+
+const server = https.createServer({key:  privateKey, cert: certificate}, app);
 const PORT = process.env.PORT || 3000;
 app.set('port', PORT);
 server.listen(PORT);
