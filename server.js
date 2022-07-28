c879e751ef95c15c1d51435c4b0369ad2a383696
Andrei Balzan <andreibalzan@gmail.com>
2022-07-28 08:04:36 +0100

    removing SSL as the host will manage SSL for us

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const http = require('http');
 const app = require("./app");
 const fs = require("fs");
 const path = require('path');
@@ -6,7 +7,8 @@ const path = require('path');
 const privateKey = fs.readFileSync(path.resolve(__dirname, './.ssl/server.key'));
 const certificate = fs.readFileSync(path.resolve(__dirname, './.ssl/server.cert'));
 
-const server = https.createServer({key:  privateKey, cert: certificate}, app);
+//const server = https.createServer({key:  privateKey, cert: certificate}, app);
+const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 app.set('port', PORT);
 server.listen(PORT);
